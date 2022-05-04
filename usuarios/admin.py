3f19fb25df85f8224505cb4335a14bfee5697345
Lucas <lcsmoura95@gmail.com>
2022-05-04 00:06:24 -0300

    django v1

---
+++ usuarios/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+from .forms import CustomUsuarioCreateForm, CustomUsuarioChangeForm
+from .models import CustomUsuario
+
+
+@admin.register(CustomUsuario)
+class CustomUsuarioAdmin(UserAdmin):
+    add_form = CustomUsuarioCreateForm
+    form = CustomUsuarioChangeForm
+    model = CustomUsuario
+    list_display = ('email','first_name', 'last_name',  'fone', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'fone','pais' ,'estado', 'municipio', 'cep', 'rua','numero','complemento','pis',)}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+
